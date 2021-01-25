59cec6e9ed467446054be35895b406d722c1a7b7
Antonio Neto <nettoveloso@gmail.com>
2021-01-25 13:07:41 -0300

    Código da primeira aula

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,38 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import db from '../db.json';
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}  >
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            Texto
+          </Widget.Header>
+          <Widget.Content>
+            Mais texto
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/nettoveloso" />
+
+    </QuizBackground>
+  )
 }
